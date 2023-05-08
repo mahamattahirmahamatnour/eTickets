f3a7da0ed4be6e1812090db5af520c15310ba9b4
Mahamat Tahir <Mahamat Tahir@DESKTOP-IMDA1PA>
2023-05-08 12:48:46 +0200

    Ajoutez des fichiers projet.

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,39 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorsService _service;
+
+        public ActorsController(IActorsService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAll();
+            return View(data);
+        }
+        //Get: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            _service.Add(actor);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public string ProfilePictureURL { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Biographiy")]
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
